721ef213ddc4634479fff6f0a9c7199cd8d51998
xiaomalover <xiaomalover@gmail.com>
2017-06-07 12:01:37 +0800

    update

--- protected/humhub/modules/user/messages/zh_cn/behaviors_ProfileControllerBehavior.php
+++ protected/humhub/modules/user/messages/zh_cn/behaviors_ProfileControllerBehavior.php
@@ -1,6 +1,6 @@
 <?php
 return array (
   'This user account is not approved yet!' => '此帐户还有没通过审核！',
-  'You need to login to view this user profile!' => '你需要登录才能查看此用户属性！',
+  'You need to login to view this user profile!' => '你需要登录才能查看该用户的信息！',
   'User not found!' => '没找到用户',
 );
